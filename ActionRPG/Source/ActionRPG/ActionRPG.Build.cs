d1c1e03170a7c087437ebf10e53270826350a59d
jinabro <kmy4869@naver.com>
2022-05-21 09:07:56 +0900

    폴더 정리

--- ActionRPG/Source/ActionRPG/ActionRPG.Build.cs
+++ ActionRPG/Source/ActionRPG/ActionRPG.Build.cs
@@ -14,10 +14,17 @@ public class ActionRPG : ModuleRules
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
+		PrivateIncludePaths.Add("ActionRPG");
+		PrivateIncludePaths.Add("ActionRPG/AI");
+		PrivateIncludePaths.Add("ActionRPG/Boss");
+		PrivateIncludePaths.Add("ActionRPG/Enemy");
+		PrivateIncludePaths.Add("ActionRPG/Environment");
+		PrivateIncludePaths.Add("ActionRPG/ETC");
+		PrivateIncludePaths.Add("ActionRPG/Item");
+		PrivateIncludePaths.Add("ActionRPG/MainCharacter");
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
 }
